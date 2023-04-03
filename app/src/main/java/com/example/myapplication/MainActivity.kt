bc315a0a550a71e71961fb28c9ad2fd303c8097c
leaisrevolution <leaisrevolution@gmail.com>
2023-04-03 16:06:55 +0900

    Box, fillMaxSize, padding

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,19 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(modifier = Modifier.background(color = Color.Green)) {
+            Box(modifier = Modifier.background(color = Color.Green)
+                .fillMaxWidth()
+                .height(200.dp)
+            ) {
                 Text("Hello")
-                Text("Hello")
+                Box(
+                    modifier = Modifier.fillMaxSize()
+                        .padding(all = 16.dp),
+                    contentAlignment = Alignment.BottomEnd
+                ) {
+                    Text("hiiiiiiiii~~~")
+                }
             }
 //            MyApplicationTheme {
 //                // A surface container using the 'background' color from the theme
