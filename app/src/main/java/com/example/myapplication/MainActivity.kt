04115f44bd6d092e7d6b01d373f5c4ca08e2d533
leaisrevolution <leaisrevolution@gmail.com>
2023-04-03 15:39:00 +0900

    background Color => * import compose.ui.graghics.Color *

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,35 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import com.example.myapplication.ui.theme.MyApplicationTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    color = MaterialTheme.colors.background
-                ) {
-                    Column(modifier = Modifier
-                        .fillMaxSize()
-                        .background(color = Color.Blue).padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally, ) {
-                        Text("Hello")
-                        Spacer(Modifier.width(16.dp))
-                        Text("world")
-                    }
-                }
+            Box(modifier = Modifier.background(color = Color.Green)) {
+                Text("Hello")
+                Text("Hello")
             }
+//            MyApplicationTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    color = MaterialTheme.colors.background
+//                ) {
+//                    Column(modifier = Modifier
+//                        .fillMaxSize()
+//                        .background(color = Color.Green).padding(16.dp)) {
+//                        Text("Hello")
+//                        Spacer(Modifier.width(16.dp))
+//                        Text("world")
+//                    }
+//                }
+//            }
         }
     }
 }
